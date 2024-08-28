0b089bc169f0af66049eba5c3ddb45e7af1a5bfc
CodingForEntrepreneurs <hello@teamcfe.com>
2024-08-28 12:24:32 -0600

    16 - Storing Chat Messages in Memory

--- reflex_gpt/chat/state.py
+++ reflex_gpt/chat/state.py
@@ -1,18 +1,37 @@
 # import time
+from typing import List
 import asyncio
 import reflex as rx
 
+class ChatMessage(rx.Base):
+    message: str
+    is_bot: bool = False
+
+
 class ChatState(rx.State):
     did_submit: bool = False
+    messages: List[ChatMessage] = []
 
     @rx.var
     def user_did_submit(self) -> bool:
         return self.did_submit
+    
+    def append_message(self, message, is_bot:bool=False):
+        self.messages.append(
+            ChatMessage(
+                message=message,
+                is_bot = False
+            )
+        )
 
     async def handle_submit(self, form_data:dict):
         print('here is our form data', form_data)
-        self.did_submit = True
-        yield
-        await asyncio.sleep(2)
-        self.did_submit = False
-        yield
+        user_message = form_data.get('message')
+        if user_message:
+            self.did_submit = True
+            self.append_message(user_message, is_bot=False)
+            yield
+            await asyncio.sleep(2)
+            self.did_submit = False
+            self.append_message(user_message, is_bot=True)
+            yield
